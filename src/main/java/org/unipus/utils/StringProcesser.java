b477f34881120420704ba08d1433aa10637da4fa
Duster Cule <79182496+Duster-Cule@users.noreply.github.com>
2025-03-22 12:23:23 +0800

    fix something

--- src/main/java/org/unipus/utils/StringProcesser.java
+++ src/main/java/org/unipus/utils/StringProcesser.java
@@ -5,7 +5,7 @@ public class StringProcesser {
     public static String processIlligalCharacter(String string) {
         // 定义允许的字符集（确保转义正确）：
 
-        String y = "a-zA-ZёЁÀ-ÿ0-9一-龥Ѐ–ӿ\uFF00-\uFFEF\uFFF0-\uFFFF　 ⼀-\u2FDF㐀-䶵豈-\uFAFF\uE863가-\uD7AFᄀ-ᇿ\u3130-\u318Fꥠ-\uA97Fힰ-\uD7FF\u3040-ゟ゠-ヿㇰ-ㇿĀ-ſ\u0080-ÿḀ-ỿ„€「」『』々ヽヾ\\[],\\.\\{}\\(\\)<>?/\\|`~\"'@#$%&*《》（）+=_￥¥。，、;:：；【】…？！!”“’‘•·\n\t\\\\-\\s";
+        String y = "a-zA-ZёЁÀ-ÿ0-9一-龥Ѐ–ӿ\uFF00-\uFFEF\uFFF0-\uFFFF　 ⼀-\u2FDF㐀-䶵豈-\uFAFF\uE863가-\uD7AFᄀ-ᇿ\u3130-\u318Fꥠ-\uA97Fힰ-\uD7FF\u3040-ゟ゠-ヿㇰ-ㇿĀ-ſ\u0080-ÿḀ-ỿ„€「」『』々ヽヾ\\[],\\.\\{}\\(\\)<>?/\\|`~\"'@#\\$%&*《》（）+=_￥¥。，、;:：；【】…？！!”“’‘•·\n\t\\\\-\\s";
 
         String regex = "[^" + y + "]" ;
 
--- src/test/java/test.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-import java.util.function.Function;
-
-public class test {
-
-    public static void main(String[] args) {
-        System.out.println(new Scanner(System.in).nextLine());
-    }
-}
